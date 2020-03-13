ac8c6109d1c90a7e52bf9d6d9199f540ba08b913
Wang Siman <m15999522089@163.com>
2020-03-13 02:02:42 -0700

    Generated clien-side SDk

--- Site/js/config.js
+++
@@ -1,3 +0,0 @@
-var ChatApp = window.ChatApp || {};
-
-ChatApp.apiEndpoint = 'https://wjgnk7946c.execute-api.us-west-1.amazonaws.com/production';
--- js/config.js
+++ js/config.js
@@ -1,1 +1,3 @@
 var ChatApp = window.ChatApp || {};
+
+ChatApp.apiEndpoint = 'https://wjgnk7946c.execute-api.us-west-1.amazonaws.com/production';
